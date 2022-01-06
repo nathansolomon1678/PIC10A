0b4b887c8eaab2a131b92ac257fe6dbf048e8b44
Nathan Solomon <nathansolomon1678@gmail.com>
2022-01-06 11:08:14 -0800

    Finish HW 1

---
+++ HW1/quote.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world! My name is Nathan.\n"
+              << "\n"
+              << "One of my favorite quotes is:\n"
+              << "\n"
+              << "\"Gotta have opposites, light and dark and dark and light, in painting. "
+              << "It's like in life. Gotta have a little sadness once in awhile so you know when the good times come.\""
+              << " ~Bob Ross\n"
+              << "\n"
+              << "I like this because it reminds me that being sad isn't purely bad.\n";
+    return 0;
+}
